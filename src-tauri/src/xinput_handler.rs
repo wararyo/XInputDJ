6570b006453c61b7359274692ad179ca76a541e3
wararyo <wararyo@gmail.com>
2025-02-08 22:57:14 +0900

    リファクタリング

--- src-tauri/src/xinput_handler.rs
+++ src-tauri/src/xinput_handler.rs
@@ -7,47 +7,64 @@ lazy_static::lazy_static! {
     static ref RUNNING: Arc<Mutex<bool>> = Arc::new(Mutex::new(false));
 }
 
+// XInputからの入力の受け取りを開始する
 pub fn start_xinput_thread(app_handle: AppHandle) {
     let running = Arc::clone(&RUNNING);
-    // すでにスレッドが動いている場合は何もしない
-    if *running.lock().unwrap() {
-        return;
+    {
+        // すでにスレッドが動いている場合は何もしない
+        let mut guard = running.lock().unwrap();
+        if *guard {
+            return;
+        }
+        *guard = true;
     }
-    *running.lock().unwrap() = true;
+
     thread::spawn(move || {
-        let handle = XInputHandle::load_default().unwrap();
+        let handle = match XInputHandle::load_default() {
+            Ok(h) => h,
+            Err(e) => {
+                app_handle.emit_to("main", "gamepad_error", format!("Failed to initialize XInput: {:?}", e)).unwrap();
+                return;
+            }
+        };
 
-        // Quick rumble test. Note that the controller might not _have_ rumble.
-        println!("rumble on: {:?}", handle.set_state(0, 1000, 1000));
-        thread::sleep(std::time::Duration::from_millis(160));
-        println!("rumble off: {:?}", handle.set_state(0, 0, 0));
+        let mut consecutive_errors = 0;
+        const MAX_ERRORS: u32 = 5; // この回数だけエラーが続いたらコントローラーが切断されたとみなす
 
-        // Show stick values, loop until the button is pressed to stop.
         loop {
             if !*running.lock().unwrap() {
-                println!("end");
                 break;
             }
+
             thread::sleep(std::time::Duration::from_millis(16));
             match handle.get_state(0) {
                 Err(e) => {
-                    println!("xinput_get_state error: {:?}", e);
-                    break;
+                    consecutive_errors += 1;
+                    if consecutive_errors >= MAX_ERRORS {
+                        app_handle.emit_to("main", "gamepad_error", format!("Controller disconnected: {:?}", e)).unwrap();
+                        break;
+                    }
                 }
                 Ok(state) => {
+                    consecutive_errors = 0;
                     if state.east_button() {
                         break;
                     } else {
                         let left_stick = state.left_stick_normalized();
                         let right_stick = state.right_stick_normalized();
-                        app_handle.emit_to("main", "gamepad_input", (left_stick, right_stick)).unwrap();
+                        if let Err(e) = app_handle.emit_to("main", "gamepad_input", (left_stick, right_stick)) {
+                            eprintln!("Failed to emit gamepad input: {:?}", e);
+                        }
                     }
                 }
             }
         }
+
+        *running.lock().unwrap() = false;
     });
 }
 
+// XInputからの入力の受け取りを停止する
 pub fn stop_xinput_thread() {
     let mut running = RUNNING.lock().unwrap();
     *running = false;
